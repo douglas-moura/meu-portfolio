8701ea7974d091c43dc97e9a6c4b9b6b4c4f7fc8
Douglas Moura <douglas12345.dsm@gmail.com>
2024-09-06 23:17:45 -0300

    Removendo arquivos antigos

--- assets/js/contato.js
+++
@@ -1,9 +0,0 @@
-function abrirMenu() {
-    let menu = document.querySelector("aside#menu-lateral")
-    if(menu.style.marginLeft == '0px') {
-        menu.style.marginLeft = '-30rem'
-    } else {
-        menu.style.marginLeft = '0px'
-    }
-    console.log(menu)
-}
