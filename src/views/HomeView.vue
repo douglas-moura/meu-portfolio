68d90113f837b3719ec6c91ea54650a0ca1285e2
Douglas Moura <douglas12345.dsm@gmail.com>
2024-09-18 16:12:44 -0300

    Adiciona arquivos não rastreados antes do merge

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,47 +1,43 @@
 <template>
-    <main id="pag-home">
-        <section>
-            <div>
-                <div id="minhas-infos">
+    <main>
+        <section id="sobre-mim" class="sessao-default">
+            <div class="linha-sessao grid-cols-3 gap-8">
+                <div class="col-conteudo col-span-2 justify-center minhas-infos-">
                     <h1>Douglas Moura</h1>
-                    <h5>_Desenvolvedor Web Front-End - São Paulo | SP</h5>
-                    <br>
-                    <p>Lorem ipsum dolor sit amet, consecbtetur adipisicing elit. Debitis, quia nesciunt vel similique atque, sunt unde delectus distinctio error dignissimos dolore? Repellat, voluptatem modi expedita pariatur itaque quod alias. Blanditiis.</p>
-                    <br>
-                    <span id="contatos">
-                        <Icon icon="akar-icons:envelope" height="1.2rem" />
-                        <p>douglas12345.dsm@gmail.com</p>
-                        <Icon icon="akar-icons:whatsapp-fill" height="1rem" />
-                        <p>+55 11 95305-5285</p>
+                    <h5 class="mb-4 text-orange-500 font-extrabold">_Desenvolvedor Web Front-End - São Paulo | SP</h5>
+                    <p class="xl:w-11/12 2xl:w-4/5 mb-8">Sou um desenvolvedor frontend apaixonado por criar interfaces digitais intuitivas e eficientes. Atualmente, foco meus conhecimentos no JavaScript e seu framework Vue Js 3. Sempre em busca de novos aprendizados, meu objetivo é aprimorar continuamente minhas habilidades e trazer experiências únicas para a web.</p>
+                    <span class="flex gap-2">
+                        <p class="mr-4 p-4 border-cinza-escuro border-4 rounded-full flex">
+                            <Icon class="mr-4" icon="akar-icons:envelope" height="1.2rem" />
+                            douglas12345.dsm@gmail.com
+                        </p>
+                        <p class="mr-4 p-4 border-cinza-escuro border-4 rounded-full flex">
+                            <Icon class="mr-4" icon="akar-icons:whatsapp-fill" height="1rem" />
+                            +55 11 95305-5285
+                        </p>
                     </span>
+                    <div class="linha-sessao grid-cols-10 mt-20">
+                        <Icon class="xl:size-8 2xl:size-10" icon="akar-icons:html-fill" />
+                        <Icon class="xl:size-8 2xl:size-10" icon="akar-icons:css-fill" />
+                        <Icon class="xl:size-8 2xl:size-10" icon="akar-icons:javascript-fill" />
+                        <Icon class="xl:size-8 2xl:size-10" icon="akar-icons:sass-fill" />
+                        <Icon class="xl:size-8 2xl:size-10" icon="akar-icons:vue-fill" />
+                        <!--
+                        <span><Icon icon="akar-icons:php-fill" /></span>
+                        <span><Icon icon="bi:filetype-sql" /></span>
+                        <span><Icon icon="akar-icons:vscode-fill" /></span>
+                        <span><Icon icon="akar-icons:github-fill" /></span>
+                        -->
+                    </div>
                 </div>
-                <picture>
-                    <CirculoCmp
-                        top=-1.5
-                        left=-2
-                        largura=100
-                        tipo="frente"
-                        cor="laranja"
-                    />
+                <div class="relative">
+                    <div class="w-full absolute rounded-full border-4 border-laranja aspect-square -top-6 -left-4 animate-slide-right"></div>
                     <ImagemCmp
                         src="foto-perfil-2"
                         formato="redondo"
                         textoAlt="Minha Foto"
                     />
-                </picture>
-            </div>
-            <div id="tecnologias">
-                <span><Icon icon="akar-icons:html-fill" /></span>
-                <span><Icon icon="akar-icons:css-fill" /></span>
-                <span><Icon icon="akar-icons:javascript-fill" /></span>
-                <span><Icon icon="akar-icons:sass-fill" /></span>
-                <span><Icon icon="akar-icons:vue-fill" /></span>
-                <!--
-                <span><Icon icon="akar-icons:php-fill" /></span>
-                <span><Icon icon="bi:filetype-sql" /></span>
-                <span><Icon icon="akar-icons:vscode-fill" /></span>
-                <span><Icon icon="akar-icons:github-fill" /></span>
-                -->
+                </div>
             </div>
         </section>
     </main>
@@ -49,60 +45,11 @@
 
 <script setup>
     import ImagemCmp from '@/components/partials/ImagemCmp.vue'
-    import CirculoCmp from '@/components/partials/CirculoCmp.vue'
     import { Icon } from '@iconify/vue'
 </script>
 
 <style scoped lang="scss">
-    #pag-home {
-        section {
-            width: 70%;
-
-            div {
-                #minhas-infos {
-                    display: flex;
-                    flex-direction: column;
-                    justify-content: center;
-                    position: relative;
-                    width: 60%;
-
-                    #contatos {
-                        display: flex;
-                        align-items: center;
-
-                        svg {
-                            margin-right: .5rem;
-                        }
-
-                        p {
-                            margin-right: 2rem;
-                        }
-                    }
-                }
-
-                picture {
-                    width: 30%;
-                    position: relative;
-
-                    #circulo {
-                        animation-name: slideRigth;
-                        animation-duration: 2s;
-                    }
-                }
-            }
-
-            #tecnologias {
-                margin-top: 2rem;
-                width: 35% !important;
-
-                span {
-                    svg {
-                        height: 2rem;
-                        width: 2rem;
-                        stroke-width: 1;
-                    }
-                }
-            }
-        }
+    #sobre-mim {
+        @apply xl:w-4/5 2xl:w-2/3
     }
 </style>
